8523e7fa3f84abeb685a929fc1fd899a3b6faf57
Dan Nixon <dan@dan-nixon.com>
2024-04-30 20:32:32 +0100

    Add client examples

---
+++ client/examples/schedule.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = Url::parse("https://www.emfcamp.org/schedule/2022.json")?;
+
+    let client = emfcamp_schedule_api::Client::new(url);
+
+    let schedule = client.get_schedule().await?;
+
+    println!("Found {} events", schedule.events.len());
+    for event in schedule.events {
+        println!("- {:?}", event);
+    }
+
+    Ok(())
+}
---
+++ client/examples/venues.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use url::Url;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let url = Url::parse("https://www.emfcamp.org/schedule/2022.json")?;
+
+    let client = emfcamp_schedule_api::Client::new(url);
+
+    let schedule = client.get_schedule().await?;
+
+    let venues = schedule.venues();
+
+    println!("Found {} venues:", venues.len());
+    for venue in venues {
+        println!("- {}", venue);
+    }
+
+    Ok(())
+}
