e90dccffefc071df3ac28b61daced83b0b51c32e
Dan Nixon <dan@dan-nixon.com>
2024-05-27 20:35:05 +0100

    Add test that also uses timezones

--- client/src/schedule/mutation/starts_before.rs
+++ client/src/schedule/mutation/starts_before.rs
@@ -50,4 +50,33 @@ mod test {
         assert_eq!(mutated[0], events[0]);
         assert_eq!(mutated[1], events[1]);
     }
+
+    #[test]
+    fn across_timezones() {
+        let events = vec![
+            Event::dummy(
+                0,
+                DateTime::parse_from_rfc3339("2024-03-12T20:00:00+01:00").unwrap(),
+            ),
+            Event::dummy(
+                1,
+                DateTime::parse_from_rfc3339("2024-03-12T21:00:00+01:00").unwrap(),
+            ),
+            Event::dummy(
+                2,
+                DateTime::parse_from_rfc3339("2024-03-12T22:00:00+01:00").unwrap(),
+            ),
+        ];
+
+        let mutator =
+            StartsBefore::new(DateTime::parse_from_rfc3339("2024-03-12T20:59:59+00:00").unwrap());
+
+        let mut mutated = events.clone();
+        mutator.mutate(&mut mutated);
+
+        assert_eq!(mutated.len(), 2);
+
+        assert_eq!(mutated[0], events[0]);
+        assert_eq!(mutated[1], events[1]);
+    }
 }
